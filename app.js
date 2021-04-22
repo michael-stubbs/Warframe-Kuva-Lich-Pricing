e69c86c954dbf157672cecafa6b04fbf7025a1e1
Michael Stubbs <michaelstubbsdev@gmail.com>
2021-04-22 15:42:11 -0700

    added Pug + test file, reworked landing page CSS

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const pug = require("pug");
 const cors = require("cors");
 const got = require("got");
 const port = 3000;
@@ -21,8 +22,11 @@ app.listen(port, () => {
 app.post("/", async function (req, res) {
   let generatedGetString = process.formAPIString(req.body);
   await callAPI(generatedGetString);
-  res.sendFile(path.join(__dirname + "/public/results.html"));
+  // res.sendFile(path.join(__dirname + "/public/results.html"));
+  res.send(compiledFunction());
 });
+
+const compiledFunction = pug.compileFile("views/results.pug");
 
 // This wouldn't work as a module. I still have no idea why results would export before it was written.
 async function callAPI(fullrequest) {
