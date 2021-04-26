38cea2905feed49238cae05bd6f5e801f66a64ff
Michael Stubbs <michaelstubbsdev@gmail.com>
2021-04-26 12:24:27 -0700

    added copy confirmation

--- public/javascript/results.js
+++ public/javascript/results.js
@@ -1,10 +1,14 @@
 function copyWhisper() {
   let body = document.getElementById("copyBody");
+  let button = document.getElementById("copyButton");
   body = body.innerHTML;
   document.execCommand("copy");
   navigator.clipboard.writeText(body).then(
     function () {
-      /* clipboard successfully set */
+      button.innerHTML = "Copied!";
+      setTimeout(function () {
+        button.innerHTML = "Copy";
+      }, 1300);
     },
     function () {
       /* clipboard write failed */
