ada0bbf4305953e1b6c7692d5bda0baf6d3b84c5
Michael Stubbs <michaelstubbsdev@gmail.com>
2021-04-26 17:16:18 -0700

    added rough hide/show offline button

--- public/javascript/results.js
+++ public/javascript/results.js
@@ -28,3 +28,33 @@ if (tableBody.rows.length < 2) {
   tableBody.deleteTHead();
   jumbotron.classList.remove("hidden");
 }
+
+// Extremely rough way to process hiding/removing entries the table.
+function hideOffline() {
+  let whisperButton = document.querySelectorAll(".whisper-button");
+  for (let i = 0; i < whisperButton.length; i++) {
+    if (whisperButton[i].classList.contains("offline")) {
+      let parentNode = whisperButton[i].parentElement;
+      parentNode = parentNode.parentElement;
+      parentNode.classList.add("hidden");
+    }
+  }
+  document.getElementById("hide-offline").classList.add("hidden");
+  document.getElementById("show-offline").classList.remove("hidden");
+}
+
+function showOffline() {
+  let whisperButton = document.querySelectorAll(".whisper-button");
+  for (let i = 0; i < whisperButton.length; i++) {
+    if (whisperButton[i].classList.contains("offline")) {
+      let parentNode = whisperButton[i].parentElement;
+      parentNode = parentNode.parentElement;
+      parentNode.classList.remove("hidden");
+    }
+  }
+  document.getElementById("show-offline").classList.add("hidden");
+  document.getElementById("hide-offline").classList.remove("hidden");
+}
+
+document.getElementById("hide-offline").addEventListener("click", hideOffline);
+document.getElementById("show-offline").addEventListener("click", showOffline);
