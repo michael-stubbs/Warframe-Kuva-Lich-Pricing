eecb15685bcfaf3d284d1ad9fb7a780994d0f1a7
Michael Stubbs <michaelstubbsdev@gmail.com>
2021-04-29 14:18:01 -0700

    Refactor API string function

--- Services/process.js
+++ Services/process.js
@@ -1,30 +1,22 @@
-function omitBlanks(field, string) {
-  if (string === "") {
-    field = "";
-    return field;
+function omitBlanks(url, result) {
+  if (result === "") {
+    return "";
   } else {
-    return field;
+    return url + result;
   }
 }
 
 function formAPIString(postedJSON) {
   let apiLink = "https://api.warframe.market/v1/auctions/search?type=lich";
-  let field1 = "&weapon_url_name=" + postedJSON.weapon;
-  field1 = omitBlanks(field1, postedJSON.weapon);
-  let field2 = "&element=" + postedJSON.element;
-  field2 = omitBlanks(field2, postedJSON.element);
-  let field3 = "&ephemera=" + postedJSON.ephemera;
-  field3 = omitBlanks(field3, postedJSON.ephemera);
-  let field4 = "&quirk=" + postedJSON.quirk;
-  field4 = omitBlanks(field4, postedJSON.quirk);
-  let field5 = "&sort_by=" + postedJSON.sorting;
-  field5 = omitBlanks(field5, postedJSON.sorting);
-  let field6 = "&buyout_policy=" + postedJSON.auctiontype;
-  field6 = omitBlanks(field6, postedJSON.auctiontype);
-  let platformHead = postedJSON.platform;
+  let field1 = omitBlanks("&weapon_url_name=", postedJSON.weapon);
+  let field2 = omitBlanks("&element=", postedJSON.element);
+  let field3 = omitBlanks("&ephemera=", postedJSON.ephemera);
+  let field4 = omitBlanks("&quirk=", postedJSON.quirk);
+  let field5 = omitBlanks("&sort_by=", postedJSON.sorting);
+  let field6 = omitBlanks("&buyout_policy=", postedJSON.auctiontype);
 
   apiLink = apiLink + field1 + field2 + field3 + field4 + field5 + field6;
-  return [apiLink, platformHead];
+  return [apiLink, postedJSON.platform];
 }
 
 function prepJSON(json) {
@@ -38,6 +30,5 @@ function prepJSON(json) {
 }
 
 //exports vs module.exports doesn't seem to matter
-exports.omitBlanks = omitBlanks;
 exports.formAPIString = formAPIString;
 exports.prepJSON = prepJSON;
